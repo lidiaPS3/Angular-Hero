965490b43f3a02c4b07ea75ff57b6447e190bd22
lidiaPS3 <lidiaperezsan@gmail.com>
2021-10-13 11:40:09 +0200

    proyecto_hero

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Capitan America' },
+  { id: 2, name: 'Viuda Negra' },
+  { id: 3, name: 'Spiderman' },
+  { id: 4, name: 'Iron Man' },
+  { id: 5, name: 'hulk' },
+  { id: 6, name: 'bLaCk PaNtEr' },
+  { id: 7, name: 'Capitana Marbel' },
+  { id: 8, name: 'Thor' },
+  { id: 9, name: 'Doctor Strange' },
+  { id: 10, name: 'Deadpool' }
+];
